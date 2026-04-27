ef1f10bc4fc297346627fb4e3304dab214604912
fqurbani730-bit <fqurbani730@gmail.com>
2026-04-27 08:36:30 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import { Header } from "./components/Header";
+import { Hero } from "./components/hero";
+import { SectionOne } from "./components/sectionOne";
+import { SectionTwo } from "./components/sectionTwo";
+import { Pictures } from "./components/pictures";
+import { ContectUs } from "./components/contactUs";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <SectionOne />
+      <SectionTwo />
+      <Pictures />
+      <ContectUs />
+    </div>
+  );
+}
+export default App;
